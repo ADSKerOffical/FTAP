cdae7f2af5ed8fd5ef74652aa20de074c9bd9d27
ADSKerOffical <137511182+ADSKerOffical@users.noreply.github.com>
2025-03-08 16:28:51 +0300

    Update Source.lua

--- Source.lua
+++ Source.lua
@@ -10,17 +10,21 @@ local Tab = Window:MakeTab({
 
 anti = hookmetamethod(game, "__namecall", newcclosure(function(self, ...)
    if not checkcaller() and self == game.Players.LocalPlayer and table.find({"destroy", "kick", "remove"}, getnamecallmethod():lower()) then
-  warn("Budgify: an attempt to destroy a player has been eliminated")
+  warn("Budgie Hub: an attempt to destroy a player has been eliminated")
 return nil
-  elseif not checkcaller() and self == game.Players ans table.find({"banasync", "destroy", "remove"}, getnamecallmethod():lower()) then
+  elseif not checkcaller() and self == game.Players and table.find({"banasync", "destroy", "remove"}, getnamecallmethod():lower()) then
    warn("Budgify: TEST1")
   return nil
     end
    return anti(self, ...)
 end))
 
-getgenv().isnetworkowner = newcclosure(function(part)
-  return (part.ReceiveAge == 0 and gethiddenproperty(part, "NetworkIsSleeping") == false)
+getgenv().isnetworkownerp = newcclosure(function(plr)
+  return (plr.Character.HumanoidRootPart.ReceiveAge == 0 and gethiddenproperty(plr.Character.HumanoidRootPart, "NetworkIsSleeping") == false) and plr.Character.Head:FindFirstChild("PartOwner") and plr.Character.Head.PartOwner.Value == game.Players.LocalPlayer.Name
+end)
+
+getgenv().isnetworkownerm = newcclosure(function(model)
+  return (model.HumanoidRootPart.ReceiveAge == 0 and gethiddenproperty(model.HumanoidRootPart, "NetworkIsSleeping") == false) and model.Head:FindFirstChild("PartOwner") and model.Head.PartOwner.Value == game.Players.LocalPlayer.Name
 end)
 
 local function sno(player, cf)
@@ -168,14 +172,45 @@ Tab:AddToggle({
  Name = "Players Kill grab",
  Default = false,
  Callback = function(Value)
+ uhgy = Value
    for _, button in next, game.Players.LocalPlayer.PlayerGui.ContextActionGui.ContextButtonFrame:GetChildren() do
   if button:IsA("ImageButton") and button.Name == "ContextActionButton" then
     button.MouseButton1Click:Connect(function()
 for _, player in next, game.Players:GetPlayers() do
-   if player ~= game.Players.LocalPlayer and player.Character and player.Character:FindFirstChild("HumanoidRootPart") and isnetworkowner(player.Character.HumanoidRootPart) and Value == true then
+pcall(function()
+   if player ~= game.Players.LocalPlayer and player.Character and player.Character.HumanoidRootPart and isnetworkownerp(player) and ughy == true then
 game.Players.LocalPlayer.SimulationRadius = math.huge
 player.Character.Humanoid.Health = 0
   end
+   end)
+end
+    end)
+  end
+   end
+ end    
+})
+
+Tab:AddToggle({
+ Name = "Players deadly grab",
+ Default = false,
+ Callback = function(Value)
+ bvg = Value
+   for _, button in next, game.Players.LocalPlayer.PlayerGui.ContextActionGui.ContextButtonFrame:GetChildren() do
+  if button:IsA("ImageButton") and button.Name == "ContextActionButton" then
+    button.MouseButton1Click:Connect(function()
+for _, player in next, game.Players:GetPlayers() do
+ pcall(function()
+   if player ~= game.Players.LocalPlayer and player.Character and player.Character.HumanoidRootPart and isnetworkownerp(player) and bvg == true then
+game.Players.LocalPlayer.SimulationRadius = math.huge
+
+for _, part in next, workspace.Map:GetDescendants() do
+  if part:IsA("BasePart") and part:FindFirstChild("TouchInterest") then
+firetouchinterest(player.Character.HumanoidRootPart, part, 0)
+  end
+end
+
+  end
+ end)
 end
     end)
   end
@@ -202,9 +237,63 @@ local args = {
 }
 
 workspace:WaitForChild(game.Players.LocalPlayer.Name .. "SpawnedInToys"):WaitForChild("CreatureBlobman"):WaitForChild("BlobmanSeatAndOwnerScript"):WaitForChild("CreatureGrab"):FireServer(unpack(args))
+humanoid.RootPart.Velocity = Vector3.zero
 task.wait(0.1)
   end
 end
+   end    
+})
+
+Tab:AddButton({
+ Name = "Kill all by using Blobman",
+ Callback = function()
+for _, humanoid in ipairs(workspace:GetDescendants()) do
+  if humanoid:IsA("Humanoid") and humanoid.Parent ~= game.Players.LocalPlayer.Character and humanoid.RootPart then
+local args = {
+    [1] = humanoid.RootPart,
+    [2] = humanoid.RootPart.CFrame
+}
+
+game:GetService("ReplicatedStorage"):WaitForChild("GrabEvents"):WaitForChild("SetNetworkOwner"):FireServer(unpack(args))
+  task.wait()
+local args = {
+    [1] = workspace:WaitForChild(game.Players.LocalPlayer.Name .. "SpawnedInToys"):WaitForChild("CreatureBlobman"):WaitForChild("LeftDetector"),
+    [2] = humanoid.RootPart,
+    [3] = workspace:WaitForChild(game.Players.LocalPlayer.Name .. "SpawnedInToys"):WaitForChild("CreatureBlobman"):WaitForChild("LeftDetector"):WaitForChild("LeftWeld")
+}
+
+workspace:WaitForChild(game.Players.LocalPlayer.Name .. "SpawnedInToys"):WaitForChild("CreatureBlobman"):WaitForChild("BlobmanSeatAndOwnerScript"):WaitForChild("CreatureGrab"):FireServer(unpack(args))
+humanoid.RootPart.Velocity = Vector3.zero
+
+task.spawn(function()
+for i = 1, 3 do 
+  task.wait(0.25)
+ if isnetworkownerp(game.Players:GetPlayerFromCharacter(humanoid.Parent)) then
+ for _, part in next, workspace.Map:GetDescendants() do
+   if part:IsA("BasePart") and part:FindFirstChild("TouchInterest") then
+ firetouchinterest(humanoid.RootPart, part, 0)
+ firetouchinterest(humanoid.RootPart, part, 1)
+   end
+ end
+end
+ end
+end)
+
+task.wait(0.1)
+  end
+end
+   end    
+})
+
+Tab:AddButton({
+ Name = "Buff blob man",
+ Callback = function()
+      workspace:FindFirstChild(game.Players.LocalPlayer.Name .. "SpawnedInToys").CreatureBlobman:FindFirstChildOfClass("Humanoid").WalkSpeed = 75
+      for _, part in next, workspace:FindFirstChild(game.Players.LocalPlayer.Name .. "SpawnedInToys").CreatureBlobman:GetDescendants() do
+        if part:IsA("BasePart") then
+part.CustomPhysicalProperties = PhysicalProperties.new(100, 100, 100)
+        end
+      end
    end    
 })
 
@@ -221,13 +310,17 @@ Tab:AddToggle({
 fgt = Value
   while fgt and task.wait() do
    game:GetService("ReplicatedStorage"):WaitForChild("CharacterEvents"):WaitForChild("Struggle"):FireServer()
-    if game.Players.LocalPlayer.Character.Humanoid.Sit then
+    if game.Players.LocalPlayer.Character.Humanoid.Sit and game.Players.LocalPlayer.Character.Head:FindFirstChild("PartOwner") then
  task.spawn(function()
   if game.Players.LocalPlayer.Character.HumanoidRootPart.Anchored then return end
 game.Players.LocalPlayer.Character.HumanoidRootPart.Anchored = true
 game.Players.LocalPlayer.Character.HumanoidRootPart.Velocity = Vector3.zero
+  if game.Players.LocalPlayer.Character.Head:FindFirstChild("PartOwner") then
+game.Players.LocalPlayer.Character.Head.PartOwner:Destroy()
+  end
  task.wait(1)
 game.Players.LocalPlayer.Character.HumanoidRootPart.Anchored = false
+game.Players.LocalPlayer.Character.HumanoidRootPart.AssemblyLinearVelocity = Vector3.zero
 end)
    end
   end
@@ -253,12 +346,125 @@ Tab:AddToggle({
  Callback = function(Value)
  loo = Value
     while loo and task.wait() do
+     pcall(function()
  local uselessly = {"BodyVelocity", "BodyThrust", "BodyAngularVelocity", "BodyGyro", "BodyForce", "RocketPropulsion", "LineForce", "VectorForce", "AngularVelocity", "RopeConstraint"}
   for _, inst in ipairs(game.Players.LocalPlayer.Character:GetDescendants()) do
     if table.find(uselessly, inst.ClassName) then
      inst:Destroy()
    end
  end
+ 
+ for _, child in pairs(game.Players.LocalPlayer.Character:GetChildren()) do
+    if child:IsA("BasePart") then
+      child.Massless = false
+      child.CustomPhysicalProperties = PhysicalProperties.new(1, 0.3, 0.5) 
+    end
+  end
+   end)
        end
+   end    
+})
+
+Tab:AddToggle({
+ Name = "Anti hack grab",
+ Default = false,
+ Callback = function(Value)
+aahhgg = Value
+     while aahhgg and task.wait() do
+       pcall(function()
+ game:GetService("ReplicatedStorage"):WaitForChild("CharacterEvents"):WaitForChild("Struggle"):FireServer()
+ game.Players.LocalPlayer.Character.Humanoid.Ragdolled.Value = false
+ game.Players.LocalPlayer.HeldTimer.Value = 0
+ game.Players.LocalPlayer.MaxHeldTimer.Value = 0
+ game.Players.LocalPlayer.IsHeld.Value = false
+ 
+    if game.Players.LocalPlayer.Character.Humanoid.Sit and game.Players.LocalPlayer.Character.Head:FindFirstChild("PartOwner") then
+ task.spawn(function()
+  if game.Players.LocalPlayer.Character.HumanoidRootPart.Anchored then return end
+game.Players.LocalPlayer.Character.HumanoidRootPart.Anchored = true
+local savedpos = game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame
+task.spawn(function()
+ repeat task.wait()
+  game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = savedpos
+  game.Players.LocalPlayer.Character.HumanoidRootPart.AssemblyLinearVelocity = Vector3.zero
+  game.Players.LocalPlayer.Character.HumanoidRootPart.Velocity = Vector3.zero
+  game.Players.LocalPlayer.Character.HumanoidRootPart.AssemblyAngularVelocity = Vector3.zero
+  if game.Players.LocalPlayer.Character.Head:FindFirstChild("PartOwner") then
+game.Players.LocalPlayer.Character.Head.PartOwner:Destroy()
+  end
+ until game.Players.LocalPlayer.Character.HumanoidRootPart.Anchored == false
+end)
+ task.wait(1)
+game.Players.LocalPlayer.Character.HumanoidRootPart.Anchored = false
+game.Players.LocalPlayer.Character.HumanoidRootPart.AssemblyLinearVelocity = Vector3.zero
+end)
+   end
+   
+ local uselessly = {"BodyVelocity", "BodyThrust", "BodyAngularVelocity", "BodyGyro", "BodyForce", "RocketPropulsion", "LineForce", "VectorForce", "AngularVelocity", "RopeConstraint", "BallSocketConstraint"}
+  for _, inst in ipairs(game.Players.LocalPlayer.Character:GetDescendants()) do
+    if table.find(uselessly, inst.ClassName) then
+     inst:Destroy()
+    elseif inst:IsA("BasePart") then
+     inst.Massless = false
+     inst.CustomPhysicalProperties = PhysicalProperties.new(1, 0.3, 0.5)
+    elseif inst:IsA("Weld") or isnt:IsA("WeldConstraint") then
+       task.delay(1, function()
+          inst:Destroy()
+       end)
+   end
+ end
+ 
+ if game.Players.LocalPlayer.Character.HumanoidRootPart:FindFirstChild("WeldHRP") then
+game.Players.LocalPlayer.Character.HumanoidRootPart.WeldHRP:Destroy()
+ end
+
+ if game.Players.LocalPlayer.Character.HumanoidRootPart:FindFirstChild("RootJoint") then
+game.Players.LocalPlayer.Character.HumanoidRootPart.RootJoint:Destroy()
+ end
+ 
+       end)
+     end
+   end    
+})
+
+Tab:AddToggle({
+ Name = "Anti void",
+ Default = false,
+ Callback = function(Value)
+ihh = Value
+      while ihh and task.wait() do
+        pcall(function()
+   if game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame.Y <= -20 or game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame.Y >= 800 then
+game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(95, -7, -20)
+game.Players.LocalPlayer.Character.HumanoidRootPart.AssemblyLinearVelocity = Vector3.zero
+   end
+        end)
+      end
+   end    
+})
+
+Tab:AddToggle({
+ Name = "Counter",
+ Default = false,
+ Callback = function(Value)
+gyvb = Value
+     while gyvb and task.wait() do
+       pcall(function()
+         if game.Players.LocalPlayer.Character.Head:FindFirstChild("PartOwner") then
+ sno(game.Players[game.Players.LocalPlayer.Character.Head.PartOwner.Value].Character.HumanoidRootPart, game.Players[game.Players.LocalPlayer.Character.Head.PartOwner.Value].Character.HumanoidRootPart.CFrame)
+   for _, part in next, workspace.Map:GetDescendants() do
+     if part:IsA("BasePart") and part:FindFirstChild("TouchInterest") then
+firetouchinterest(game.Players[game.Players.LocalPlayer.Character.Head.PartOwner.Value].Character.HumanoidRootPart, part, 0)
+     end
+   end
+ task.delay(0.05, function()
+ungrab(game.Players[game.Players.LocalPlayer.Character.Head.PartOwner.Value].Character.HumanoidRootPart)
+ end)
+ game.Players.LocalPlayer.Character.Head.PartOwner:Destroy()
+ game.Players.LocalPlayer.Character.HumanoidRootPart.AssemblyLinearVelocity = Vector3.zero
+ game:GetService("ReplicatedStorage"):WaitForChild("CharacterEvents"):WaitForChild("Struggle"):FireServer()
+         end
+       end)
+     end
    end    
 })
